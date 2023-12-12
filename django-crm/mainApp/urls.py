0d535d12d8ef4dda6307c96668ef4efda47b08dd
effiecancode <faitheffie25@gmail.com>
2023-12-12 09:50:44 +0300

    static configuration

--- django-crm/mainApp/urls.py
+++ django-crm/mainApp/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('website.urls')),
+    path('', include('website.urls')),
 
-    # path('Account/', include('Account.urls')),
+    path('Account/', include('Account.urls')),
 
 ]
 
